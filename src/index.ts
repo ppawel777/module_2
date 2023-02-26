ad9d865da7ba180bf32b3eb32974313604a0315a
ppawel1 <ppawel1@yandex.ru>
2023-02-26 13:42:25 +0300

    total price

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,29 @@
 import './index.scss';
-import { seasons, background_enum } from './data'
 
 const ready = (): void => {
-  const main_bg = document.querySelector('.wrap') as HTMLElement;
-  main_bg && 
-  main_bg.style.setProperty('--background', `url(${background_enum.summer}) center / cover no-repeat`); // default
 
-  const buttons = document.querySelectorAll(".seasons__item")
-  const btn_array = Array.prototype.slice.call(buttons);
-  const volume_range = document.querySelector("#volume-control") as HTMLElement;
+  interface IPriceParameters {
+    price: number,
+    discount: number,
+    isInstallment: boolean,
+    months: number
+  };
 
-  const play = (id: string, i_as_arr: HTMLElement[]) => {
-    const curr_seasons = seasons.find(f => f.id === id) || null
-    if (curr_seasons) {
-      volume_range.addEventListener("change", (event: Event): void => {
-        const currentTarget = event.currentTarget as HTMLInputElement;
-        const volume: number = +currentTarget.value / 100;
-        curr_seasons.audio.volume = volume
-      })
+  const totalPrice = ({ price, discount, isInstallment, months }: IPriceParameters): number => {
+    // Your code here...
+    const total_price: number = price - (price * discount / 100);
+    return isInstallment ? total_price/months : total_price
+  };
 
-      seasons.forEach(val => {
-        if (val.id !== id) {
-          if (!val.audio.paused) {
-            const prev = document.getElementById(val.id) as HTMLElement;
-            const prev_as_arr = Array.from(prev.children[0].children) as HTMLElement[];
-            prev_as_arr.forEach(svg => {
-              if (svg.classList.contains("hide")) svg.style.display = "none"
-              if (svg.classList.contains("show")) svg.style.display = "inline"
-            })
-            val.audio.pause();
-            val.audio.currentTime = 0;
-          }
-        }
-      })
-      
-      if (curr_seasons.audio.paused) {
-        curr_seasons.audio.play()
-        
-        i_as_arr.forEach(svg => {
-          if (svg.classList.contains("show")) svg.style.display = "none"
-          if (svg.classList.contains("hide")) svg.style.display = "inline"
-        })
-      } else {
-        curr_seasons.audio.pause()
-        
-        i_as_arr.forEach(svg => {
-          if (svg.classList.contains("hide")) svg.style.display = "none"
-          if (svg.classList.contains("show")) svg.style.display = "inline"
-        })
-      }
-      main_bg && main_bg.style.setProperty('--background', `url(${curr_seasons.bg}) center / cover no-repeat`);
-    }
-  }
+  const price = 100000;
+  const discount = 25;
+  const months = 12;
 
-  btn_array.forEach(item => {
-    item.style.setProperty(`--background-${item.id}`, `url(${background_enum[item.id]}) center / cover no-repeat`);
-    const i_as_arr = Array.from(item.children[0].children) as HTMLElement[];
-    item.onclick = () => play(item.id, i_as_arr)
-  });
+  const installment_on = totalPrice({ price: price, discount: discount, isInstallment: true, months: months });
+  const installment_off = totalPrice({ price: price, discount: discount, isInstallment: false, months: months });
+
+  console.log('installment_on', installment_on);
+  console.log('installment_off', installment_off);
 }
 
 document.addEventListener("DOMContentLoaded", ready);
